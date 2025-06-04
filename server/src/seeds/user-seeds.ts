ccbb9d3674402a6e6e22de4d7cacd3138f850ab6
gamalmubarak <eng-gemy@hotmail.com>
2025-06-03 20:57:33 -0400

    update the user seed file

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -1,9 +1,13 @@
 import { User } from '../models/user.js';
 
 export const seedUsers = async () => {
-  await User.create([
+  await User.deleteMany({});
+  for (const user of [
     { username: 'JollyGuru', password: 'password' },
     { username: 'SunnyScribe', password: 'password' },
     { username: 'RadiantComet', password: 'password' },
-  ]);
+  ]) {
+    const newUser = new User(user);
+    await newUser.save(); // This triggers pre-save hook for hashing
+  }
 };
